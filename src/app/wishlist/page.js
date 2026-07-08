07863b2cfab42a30e21e33c63c86a249434b8e9b
Abhijeet Anand <117760750+Abhijeet912@users.noreply.github.com>
2026-07-08 16:31:19 +0530

    feat(account): add /wishlist page

---
+++ src/app/wishlist/page.js
@@ -0,0 +1,10 @@
+import WishlistView from "@/components/account/WishlistView";
+
+export const metadata = {
+  title: "My Wishlist",
+  description: "Pieces you've saved for later.",
+};
+
+export default function WishlistPage() {
+  return <WishlistView />;
+}
